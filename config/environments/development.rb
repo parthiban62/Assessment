83f265d380557dfa65eb90d5042dfee2100006cd
parthiban <parthibanjayaraj@gmail.com>
2019-07-11 01:33:15 +0530

    Mailer settings updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'https://assessment-test-survey.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
+    :openssl_verify_mode => OpenSSL::SSL::VERIFY_NONE,      
+    :ssl => true,
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
     :domain => 'heroku.com',
     :address => 'smtp.sendgrid.net',
-    :port => 587,
+    :port => 465,
     :authentication => :plain,
     :enable_starttls_auto => true    
   }
