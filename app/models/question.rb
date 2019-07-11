673a54e22706d857e010c1ed2e459e50817661d1
parthiban <parthibanjayaraj@gmail.com>
2019-07-11 16:24:21 +0530

    Survey questions listing page UI

--- app/models/question.rb
+++ app/models/question.rb
@@ -11,4 +11,8 @@ class Question < ApplicationRecord
 	def is_subjective?
 		question_type.name.eql?("Text") || question_type.name.eql?("TextArea")
 	end
+
+	def is_text_type?
+		question_type.name.eql?("Text")
+	end
 end
