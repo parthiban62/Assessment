1544babda11014d4aeceb41b2945a2870dcffcdd
parthiban <parthibanjayaraj@gmail.com>
2019-07-12 17:51:02 +0530

    fonts added

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,6 @@
 class Response < ApplicationRecord
 	belongs_to :user
 	belongs_to :survey
-	has_many :answers
+	has_many :answers, dependent: :destroy
 	has_many :questions
 end
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,7 +2,7 @@ class Survey < ApplicationRecord
 	belongs_to :user
 	has_many :survey_questions, dependent: :destroy
 	has_many :questions, through: :survey_questions
-	has_many :responses
+	has_many :responses, dependent: :destroy
 	has_many :shares
 	accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: proc { |attr| attr['question_content'].blank?}
 
